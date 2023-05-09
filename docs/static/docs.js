949942397103caeae2debdc52d56356edbbf7249
Md. Almas Ali <almaspr3@gmail.com>
2023-05-10 03:05:09 +0600

    Updated documentation with mkdocs.

--- docs/static/docs.js
+++
@@ -1,8 +0,0 @@
-const _starter_btn = document.getElementById("starter-btn");
-const _starter_text = document.getElementById("starter-text");
-
-_starter_btn.addEventListener("click", () => {
-  navigator.clipboard.writeText(_starter_text.innerText);
-
-  // alert("Copied to clipboard!");
-});
