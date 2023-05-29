6aadd33b3a74d2b992a42b7efa6738bcbc95ae7d
Md. Almas Ali <almaspr3@gmail.com>
2023-05-30 02:13:21 +0600

    Adding new example project for testing.

--- examples/widgets twicks/app.py
+++ examples/widgets twicks/app.py
@@ -22,34 +22,51 @@ def style() -> Element:
 
 def layout(data: Element) -> Element:
     '''This is the layout of the page.'''
-    return (
-        Html(
-            Head(
-                Meta(charset="utf-8"),
-                Title("Fronty widgets example"),
-                # Style(
-                #     style().render()
-                # ),
-            ),
-            Body(
-                Div(
-                    data,
-                ).class_("container"),
-            ),
-        )
+    return Html(
+        Head(
+            Meta(charset="utf-8"),
+            Title("Fronty widgets example"),
+            # Style(
+            #     style().render()
+            # ),
+        ),
+        Body(
+            Div(
+                data,
+            ).class_("container"),
+        ),
     )
 
 
 def login_form() -> Element:
     '''This is the login form.'''
     return layout(
-        Div(
+        Empty(
             FormWidget(
-                load_css=True,
-                action='#',
-                method='post'
-            ).render()
-        ).class_('form-tags'),
+                load_css=True
+            )
+            .add_element(
+                html.Empty(
+                    html.H1('Login form')
+                    .attr('align', 'center')
+                    .style(font_size='30px', font_family='sans-serif')
+                    .class_('form-title'),
+                    html.Text('Please fill in this form to login.').class_(
+                        'form-subtitle'),
+                ),
+                first=True
+            )
+            .add_element(
+                html.Empty(
+                    html.Anchor('Terms and conditions')
+                    .attr('href', '#')
+                    .style(color='blue', cursor='pointer', font_size='15px')
+                ),
+                first=False
+            )
+            .style(display='flex', flex_direction='row', justif_items='center')
+            .render(),
+        )
     )
 
 
@@ -57,6 +74,43 @@ def login_form() -> Element:
 def index():
     '''Widgets at a place'''
     return login_form().render()
+
+
+@app.route('/test')
+def test():
+    return Html(
+        Head(
+            Meta(charset="utf-8"),
+            Title("Test case"),
+        ),
+        Body(
+            Div(
+                Text(
+                    'This is a paragraph.'
+                )
+            )
+            .style(font_size='40px', font_family='sans-serif')
+            .class_('container'),
+
+
+            html.Div(
+                html.Input()
+                .attr('type', 'text')
+                .attr('name', 'InputElement')
+                .attr('value', 'InputValue')
+                .attr('placeholder', 'InputPlaceholder')
+                .required
+                .readonly
+                .disabled,
+            ),
+
+            Footer(
+                Text(
+                    'This is a footer.'
+                )
+            ).id('footer'),
+        ),
+    ).render()
 
 
 if __name__ == '__main__':
