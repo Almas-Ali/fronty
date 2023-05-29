7847d141e8930efbb811d86796ab496a0173029d
Md. Almas Ali <almaspr3@gmail.com>
2023-05-30 02:50:53 +0600

    Updated to 0.0.5 alpha.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='fronty',
-    version='0.0.4',
+    version='0.0.5',
     description='A frontend web framework',
     long_description_content_type='text/markdown',
     long_description=long_description,
